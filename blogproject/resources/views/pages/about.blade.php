ab33bea56830d4fe3420ecd4f1e044a420cf6977
daniichiy <ichiydani@gmail.com>
2026-01-22 17:22:34 -0400

    feat: create pages

---
+++ blogproject/resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur minus perferendis consectetur, voluptatibus consequuntur laborum soluta obcaecati illum tempora fuga possimus quaerat. Eum enim facere, libero nemo alias commodi? Illo.</p>
+@endsection
--- blogproject/routes/web.php
+++ blogproject/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/services', [PagesController::class, 'services']);
+
+
+// Route::get('/hello', function () {
+//     return '<h1>Hello World</h1>';
+// });
+
+// Route::get('/about', function () {
+//     return view('pages.about');
+// });
+
+Route::get('/users/{id}', function ($id) {
+    return 'this is a user '. $id;
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
